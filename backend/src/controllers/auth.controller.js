5a2cabe0efd6eb293e0df1a4bc9d93cdcec40def
alienfoxx <alienfoxx001@gmail.com>
2024-12-26 20:55:43 +0100

    create jwt and setup the signup auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,49 @@
-export const signup = (req, res) => {
-  
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    // hash passwords
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log('Error in signup controller', error.message);
+    res.status(500).json({message:"internal Server Error"});
+  }
 };
 
 export const login = (req, res) => {
@@ -9,3 +53,6 @@ export const login = (req, res) => {
 export const logout = (req, res) => {
   res.send("logout router");
 };
+
+
+//42:14 test api with postman
