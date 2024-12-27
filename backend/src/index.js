f90338ed61055b108392bc5e616618a09a41ac1b
alienfoxx <alienfoxx001@gmail.com>
2024-12-27 19:18:18 +0100

    create the messages router and model and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 dotenv.config();
@@ -13,7 +14,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes),
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
   app.listen(PORT, () => {
     console.log("server is running PORT: " + PORT);
     connectDB();
